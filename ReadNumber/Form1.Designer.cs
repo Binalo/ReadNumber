3b983c1a2d847465908790b0637704ca9fd5f249
Dien <dien25888@gmail.com>
2020-03-21 15:35:17 +0700

    Hola

---
+++ ReadNumber/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ReadNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textSo = new System.Windows.Forms.TextBox();
+            this.textChu = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(166, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(166, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Chữ";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(519, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Đọc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textSo
+            // 
+            this.textSo.Location = new System.Drawing.Point(226, 137);
+            this.textSo.Name = "textSo";
+            this.textSo.Size = new System.Drawing.Size(270, 22);
+            this.textSo.TabIndex = 3;
+            // 
+            // textChu
+            // 
+            this.textChu.Location = new System.Drawing.Point(226, 205);
+            this.textChu.Name = "textChu";
+            this.textChu.Size = new System.Drawing.Size(270, 22);
+            this.textChu.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textChu);
+            this.Controls.Add(this.textSo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textSo;
+        private System.Windows.Forms.TextBox textChu;
+    }
+}
+
---
+++ ReadNumber/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] Ones = { "Một", "Hai", "Ba", "Bốn", "Năm", "Sáu", "Bảy", "Tám", "Chín", "Mười", "Mười một", "Mười hai", "Mười ba", "Mười bốn", "Mười lăm", "Mười sáu", "Mười bảy", "Mười tám", "Mười chín" };
+            string[] Tens = { "Mười", "Hai mươi", "Ba mươi", "Bốn mươi", "Năm mươi", "Sáu mươi", "Bảy mươi", "Tám mươi", "Chín mươi" };
+
+            int so = int.Parse(textSo.Text);
+            string strWords = "";
+
+            if(so>999 && so < 10000)
+            {
+                int i= so/ 1000;
+                strWords = strWords + Ones[i - 1] + " Ngàn ";
+                so = so % 1000;
+            }
+            if(so>100 && so < 999)
+            {
+                int i = so / 100;
+                strWords = strWords + Ones[i - 1] + " Trăm ";
+                so = so % 100;
+            }
+            if (so>19 && so <100)
+            {
+                int i = so / 10;
+                strWords = strWords + Tens[i - 1] + " Mươi ";
+                so = so % 10;
+            }
+            if (so > 0 && so < 20)
+            {
+                strWords = strWords + Ones[so - 1] ;   
+            }
+            textChu.Text = strWords;
+
+        }
+    }
+}
